a16a473004decc0f2fe4d661035c40f726bebd21
DanielDonato <daniel_donato10@yahoo.com.br>
2019-12-02 21:49:53 -0300

    Trocando pagina pelo drawer

--- lib/tiles/drawer_tile.dart
+++ lib/tiles/drawer_tile.dart
@@ -4,8 +4,10 @@ class DrawerTile extends StatelessWidget {
 
   final IconData icon;
   final String text;
+  final PageController controller;
+  final int page;
 
-  DrawerTile(this.icon, this.text);
+  DrawerTile(this.icon, this.text, this.controller, this.page);
 
   @override
   Widget build(BuildContext context) {
@@ -13,19 +15,28 @@ class DrawerTile extends StatelessWidget {
       color: Colors.transparent,
       child: InkWell(
         onTap: () {
-          
+          Navigator.of(context).pop();
+          controller.jumpToPage(page);
         },
         child: Container(
           height: 60,
           child: Row(
             children: <Widget>[
-              Icon(icon, size: 32, color: Colors.black,),
+              Icon(
+                icon,
+                size: 32,
+                color: controller.page.round() == page
+                    ? Theme.of(context).primaryColor
+                    : Colors.grey[700],
+              ),
               SizedBox(width: 32.0,),
               Text(
                 text,
                 style: TextStyle(
                   fontSize: 16,
-                  color: Colors.black
+                  color: controller.page.round() == page
+                      ? Theme.of(context).primaryColor
+                      : Colors.grey[700],
                 ),
               )
             ],
--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -3,6 +3,11 @@ import 'package:lojavirtual/tiles/drawer_tile.dart';
 
 
 class CustomDrawer extends StatelessWidget {
+
+  final PageController pageController;
+
+  CustomDrawer(this.pageController);
+
   @override
   Widget _buildDrawerBack() => Container(
     decoration: BoxDecoration(
@@ -69,10 +74,10 @@ class CustomDrawer extends StatelessWidget {
                 ),
               ),
               Divider(),
-              DrawerTile(Icons.home, "Inicio"),
-              DrawerTile(Icons.list, "Produtos"),
-              DrawerTile(Icons.location_on, "Lojas"),
-              DrawerTile(Icons.playlist_add_check, "Meus Pedidos"),
+              DrawerTile(Icons.home, "Inicio", pageController, 0),
+              DrawerTile(Icons.list, "Produtos", pageController, 1),
+              DrawerTile(Icons.location_on, "Lojas", pageController, 2),
+              DrawerTile(Icons.playlist_add_check, "Meus Pedidos", pageController, 3),
             ],
           )
         ],
