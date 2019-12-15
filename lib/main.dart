e87b6e3235aa9bf7b500a30ab7f537d19b0e2dfe
DanielDonato <daniel_donato10@yahoo.com.br>
2019-12-15 18:24:40 -0300

    model de carrinho

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:lojavirtual/screens/login_screen.dart';
 import 'package:lojavirtual/screens/signup_screen.dart';
 import 'package:scoped_model/scoped_model.dart';
 
+import 'models/cart_model.dart';
 import 'models/user_model.dart';
 
 void main() => runApp(MyApp());
@@ -14,15 +15,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScopedModel<UserModel>(
       model: UserModel(),
-      child: MaterialApp(
-        title: "Flutter's Clothing",
-        theme: ThemeData(
-            primarySwatch: Colors.blue,
-            primaryColor: Color.fromARGB(255, 4, 125, 141)
-        ),
-        debugShowCheckedModeBanner: false,
-        home: HomeScreen(),
-      ),
+      child: ScopedModelDescendant(
+        builder: (context, child, model) {
+          return ScopedModel<CartModel>(
+            model: CartModel(model),
+            child: MaterialApp(
+              title: "Flutter's Clothing",
+              theme: ThemeData(
+                  primarySwatch: Colors.blue,
+                  primaryColor: Color.fromARGB(255, 4, 125, 141)
+              ),
+              debugShowCheckedModeBanner: false,
+              home: HomeScreen(),
+            ),
+          );
+        }
+      )
     );
   }
 }
