494fa5add56c1ee1f983db510df32b190b65a681
DanielDonato <daniel_donato10@yahoo.com.br>
2019-12-16 23:40:48 -0300

    Resumo do pedido

--- lib/models/cart_model.dart
+++ lib/models/cart_model.dart
@@ -63,5 +63,27 @@ class CartModel extends Model {
     this.discountPercentage = discountPercentage;
   }
 
+  void updatePrices() {
+    notifyListeners();
+  }
+
+  double getProductsPrice() {
+    double price = 0.0;
+    for(CartProduct c in products) {
+      if(c.productData != null) {
+        price += c.quantity * c.productData.price;
+      }
+    }
+    return price;
+  }
+
+  double getDiscount() {
+    return getProductsPrice() * discountPercentage / 100;
+  }
+
+  double getShipPrice(){
+    return 9.99;
+  }
+
   static CartModel of(BuildContext context) => ScopedModel.of<CartModel>(context);
 }
